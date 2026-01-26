a2bedd6e7aff059ae6aab5fc2d385856ba78861d
Kevin Nahum <kevin.nahum121@gmail.com>
2026-01-25 18:22:44 -0800

    forgot to push the action type registry

--- schema/ActionTypeRegistry.ts
+++ schema/ActionTypeRegistry.ts
@@ -1,1 +1,8 @@
-// This document will contain the policy for LLM Action Types
+// A strict whitelist of the ONLY 4 things the agent can do.
+
+export enum ActionType {
+  READ_FILE = "READ_FILE",
+  WRITE_FILE = "WRITE_FILE",
+  THINK = "THINK",
+  FINISH = "FINISH"
+}
